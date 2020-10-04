c04ad482e188eb7283a76c7166d3f1b28810e8ea
hitmanrec <28543962+hitmanrec@users.noreply.github.com>
2020-10-04 16:25:21 +0500

    Add files via upload
    Fixed Encapsulation

--- Program.cs
+++ Program.cs
@@ -13,11 +13,11 @@ namespace oop_TicTacToe
             TicTacToe game = new TicTacToe(5,4);
             while (!game.win)
             {
-                game.PrintGrid();
+                PrintGrid(game);
                 PrintNextTurn(game);
                 AskForInput(game);
             }
-            game.PrintGrid();
+            PrintGrid(game);
             Console.WriteLine(game.WhosTurn() + " is winner!!!");
         }
 
@@ -31,17 +31,52 @@ namespace oop_TicTacToe
             int x = 0, y = 0;
             while (true)
             {
-                Console.WriteLine("Input x, y values in range [1," + game.GetGrid().GetLength(0) + "]");
+                Console.WriteLine("Input x, y values in range [1," + game.gridSize + "]");
                 string[] s = Console.ReadLine().Split(' ');
                 x = int.Parse(s[1]);
                 y = int.Parse(s[0]);
-                if (x > 0 && x <= game.GetGrid().GetLength(0) && y > 0 && y <= game.GetGrid().GetLength(0))
-                    if (game.GetGrid()[x - 1, y - 1] == TicTacToe.mark.Z)
+                if (x > 0 && x <= game.gridSize && y > 0 && y <= game.gridSize)
+                    if (game.isEmpty(x - 1, y - 1))
                         break;
                 Console.WriteLine("Wrong input!");
             }
             game.makeTurn(x, y);
         }
-
+        static public void PrintGrid(TicTacToe game)
+        {
+            int l = game.gridSize;
+            TicTacToe.mark[,] grid = game.GetGrid();
+            Console.Write("╔");
+            for (int i = 0; i < l - 1; i++)
+            {
+                Console.Write("═╦");
+            }
+            Console.Write("═╗\n");
+            for (var i = 0; i < l; i++)
+            {
+                Console.Write("║");
+                for (var j = 0; j < l; j++)
+                {
+                    Console.Write((grid[i, j] == TicTacToe.mark.Z ? 
+                        " " : grid[i, j].ToString()) + "║");
+                }
+                if (i < l - 1)
+                {
+                    Console.Write("\n╠");
+                    for (var j = 0; j < l - 1; j++)
+                    {
+                        Console.Write("═╬");
+                    }
+                    Console.Write("═╣");
+                }
+                Console.Write("\n");
+            }
+            Console.Write("╚");
+            for (int i = 0; i < l - 1; i++)
+            {
+                Console.Write("═╩");
+            }
+            Console.Write("═╝\n");
+        }
     }
 }
--- TicTacToe.cs
+++ TicTacToe.cs
@@ -11,38 +11,45 @@ namespace oop_TicTacToe
         public enum mark { Z , X , O }
         private mark[,] grid;
         private mark turn;
+        public int gridSize { get; private set; }
         public int winCondition { get; private set; }
         public bool win { get; private set; }
 
-        public TicTacToe(int gridSize = 3, int winRowSize = 3)
+        public TicTacToe(int _gridSize = 3, int winRowSize = 3)
         {
-            if (gridSize < 3 || gridSize > 15)
+            if (_gridSize < 3 || _gridSize > 15)
                 throw new ArgumentException("Grid size must be at least 3, and less than 16!");
-            if(winRowSize < 3 || winRowSize > gridSize)
+            gridSize = _gridSize;
+            if (winRowSize < 3 || winRowSize > gridSize)
                 throw new ArgumentException("Win row size must be at least 3, and less than grid size!");
             winCondition = winRowSize;
             grid = new mark[gridSize, gridSize];
-            for (var i = 0; i < gridSize; i++)
-                for (var j = 0; j < gridSize; j++)
+            for (var i = 0; i < _gridSize; i++)
+                for (var j = 0; j < _gridSize; j++)
                     grid[i, j] = mark.Z;
             turn = mark.X;
             win = false;
         }
         public mark[,] GetGrid()
         {
-            return grid;
+            mark[,] _grid = grid.Clone() as mark[,];
+            return _grid;
         }
         public mark WhosTurn()
         {
             return turn;
+        }
+        public bool isEmpty(int x, int y)
+        {
+            return (grid[x, y] == mark.Z);
         }
         
         public void makeTurn(int x, int y)
         {
             if (!win)
             {
-                if (x > grid.GetLength(0) || x < 1 || y > grid.GetLength(0) || y < 1)
-                    throw new ArgumentException("Out of bounds! (x:[1," + grid.GetLength(0) + "] y:[1," + grid.GetLength(0) + "])");
+                if (x > gridSize || x < 1 || y > gridSize || y < 1)
+                    throw new ArgumentException("Out of bounds! (x:[1," + gridSize + "] y:[1," + gridSize + "])");
                 if (grid[x - 1, y - 1] != mark.Z)
                     throw new ArgumentException("Place already occupied!");
                 grid[x - 1, y - 1] = turn;
@@ -53,7 +60,7 @@ namespace oop_TicTacToe
         public bool CheckForWin(mark who)
         {
             //check rows & cols
-            for (int i = 0, l = grid.GetLength(0); i < l; i++)
+            for (int i = 0, l = gridSize; i < l; i++)
             {
                 int rowCounter = 0, colCounter = 0;
                 for(int j = 0; j < l; j++)
@@ -66,7 +73,7 @@ namespace oop_TicTacToe
                 }
             }
             //diagonals 
-            for(int i, j, row = winCondition - 1, col = 0, l = grid.GetLength(0); //from bottom left corner
+            for(int i, j, row = winCondition - 1, col = 0, l = gridSize; //from bottom left corner
                 col <= l - winCondition;)
             {
                 i = row;
@@ -82,7 +89,7 @@ namespace oop_TicTacToe
                 if (row < l - 1) row++;
                 else col++;
             }
-            for (int i, j, l = grid.GetLength(0), row = l - winCondition, col = 0; //from top left corner
+            for (int i, j, l = gridSize, row = l - winCondition, col = 0; //from top left corner
                 col <= l - winCondition;)
             {
                 i = row;
@@ -100,37 +107,6 @@ namespace oop_TicTacToe
             }
             return false;
         }
-        public void PrintGrid()
-        {
-            Console.Write("╔");
-            for (int i = 0; i < grid.GetLength(0)-1; i++){
-                Console.Write("═╦");
-            }
-            Console.Write("═╗\n");
-            for (var i = 0; i < grid.GetLength(0); i++)
-            {
-                Console.Write("║");
-                for (var j = 0; j < grid.GetLength(0); j++)
-                {
-                    Console.Write(grid[i, j] + "║");
-                }
-                if(i < grid.GetLength(0) - 1)
-                {
-                    Console.Write("\n╠");
-                    for (var j = 0; j < grid.GetLength(0) - 1; j++)
-                    {
-                        Console.Write("═╬");
-                    }
-                    Console.Write("═╣");
-                }
-                Console.Write("\n");
-            }
-            Console.Write("╚");
-            for (int i = 0; i < grid.GetLength(0)-1; i++)
-            {
-                Console.Write("═╩");
-            }
-            Console.Write("═╝\n");
-        }
+        
     }
 }
